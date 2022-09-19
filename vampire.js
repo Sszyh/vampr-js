f6ff16c611de7b3373807c8ed2518ef86cfb08b3
Sszyh <zhuyer@outlook.com>
2022-09-19 15:13:27 -0700

    add functions to traverse vampire

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,8 +43,49 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (!this.creator) {
+      return this;
+    }
+    if (this.numberOfVampiresFromOriginal <= 1) {
+      return this.creator;
+    }
+    if (this.name === vampire.name) {
+      return this;
+    }
 
+    if (this.creator.name === vampire.name || this.name === vampire.creator.name) {
+      if (this.creator.name === vampire.name) {
+        return vampire;
+      } else {
+        return this;
+      }
+    }
+
+    if (this.creator.name !== vampire.creator.name) {
+      let arrayOfThis = [];
+      let arrayOfVam = [];
+      let thCreator = this.creator
+      let vamCreator = vampire.creator
+      while (thCreator) {
+        arrayOfThis.push(thCreator);
+        thCreator = thCreator.creator;
+      }
+      while (vamCreator) {
+        arrayOfVam.push(vamCreator);
+        vamCreator = vamCreator.creator;
+      }
+
+      for (let c of arrayOfThis) {
+        for (let v of arrayOfVam) {
+          if (c === v) {
+            return v;
+          }
+        }
+      }
+    }
   }
+
+  
 }
 
 module.exports = Vampire;
